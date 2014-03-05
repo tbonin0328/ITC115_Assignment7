c0096066674ada5fe9721083d4d4710239146b46
Thomas B <tbonin0328@yahoo.com>
2014-03-05 15:52:58 -0800

    final turned in, with some test comments still left in, argh

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -11,18 +11,18 @@ public class GuessingGame
 	private Scanner in = new Scanner(System.in); 
 	private Random rGen = new Random();
 	
-    private static final int MAX_GUESSES = 50;
+    private static final int MAX_GUESSES = 3;
     
     private int numGames = 0;
-    private int numGuessesThis = 0;
+    public int numGuessesThis = 0;
     private int numGuessesAll = 0;
-    private int bestScore = 50;
-    private int firstAsk = 0;
+    private int bestScore = 0;
     private int goalNum = 0;
     private int yourGuess = 0;
     private String loseMessage = "Sorry, you lost this round";
     private String higherMessage = "It's higher";
     private String lowerMessage = "It's lower";
+    private String playAgain = "";
     
 	/**
 	 * This is the main play method. It sends a message to the user that 
@@ -31,16 +31,8 @@ public class GuessingGame
 	 * If user does not say "Y", then the system posts game results.
 	 */
 	public void play()
-	{
-		
-		//firstAsk = Guess();
-		//System.out.println(numGuessesThis);
-		
-		do 
-		{
-			newGame();
-		}
-		while (playAgain());
+	{	
+		newGame();
 	}
 	
 	/**
@@ -50,17 +42,30 @@ public class GuessingGame
 	 * The method then uses the numGames variable to post total games played. It 
 	 * uses numGuessesAll/numGames to post Guesses/game. It posts the bestScore
 	 * derived from the if statement at the top of this method.
+	 * @return 
 	 */
-	private void gameResults() 
+	private boolean gameResults() 
 	{
 		if (numGuessesThis <= bestScore)
 		{
-			numGuessesThis = bestScore;	
+			bestScore = numGuessesThis;	
 		}
+		
 		System.out.println("Your Overall Results:");
 		System.out.println("Total Games: " + numGames);
-		System.out.println("Guesses/game: " + numGuessesAll/numGames);
-		System.out.println("Best Score:" + bestScore);
+		
+		if (numGames != 0)
+		{
+			System.out.println("Guesses/game: " + (double)numGuessesAll/(double)numGames);
+			System.out.println("Best Score:" + bestScore);
+		}
+		
+		else 
+		{
+			printLine("Guess/game: N/A because you didn't finish your first game.");
+			printLine("Best Score: No Best Score either.");
+		}
+		return false;
 	}
 
 	/**
@@ -70,97 +75,147 @@ public class GuessingGame
 	 */
 	private void newGame() 
 	{
-		//because new game has been selected, increment numGames for gameResults method.
-		numGames++;
-		//because new game has been selected, set number of guesses for this game back to zero.
-		numGuessesThis = 0;
-		//start the guess process.
-		System.out.println("I'm thinking of a number between 1 and 100...");
-		//process first game
-		newGuess();
+			while(playAgain() && numGuessesThis != MAX_GUESSES)
+				{	
+					System.out.println("I'm thinking of a number between 1 and 100...");
+					goalNum = makeGoalNum();
+
+					while(guessCheck())
+					{
+							System.out.print("");
+					}
+				}
+			if (numGuessesThis != MAX_GUESSES)
+			{
+				gameResults();
+			}
 	}
 	
-	private void newGuess()
-	{
-		goalNum = makeGoalNum(); //20
-		
-		if (numGuessesThis <= 50)
+	/**
+	 * @return: returns goalNum if works, otherwise returns false.
+	 * This method checks to see if guess works.
+	 */
+	private boolean guessCheck() 
+	{	
+		while (totalTest())
 		{
-			while(true)
+			yourGuess = Guess();
+			
+			if (yourGuess > goalNum)
 			{
-				yourGuess = Guess(); //yourGuess = 6
-	
+				printLine(lowerMessage);
+				System.out.println(goalNum + ", " + numGuessesThis + ", " + numGuessesAll + ", " + numGames);
+				guessesLeft();
+			}
+			else if (yourGuess < goalNum)
+			{
+				printLine(higherMessage);
+				System.out.println(goalNum + ", " + numGuessesThis + ", " + numGuessesAll + ", " + numGames);
+				guessesLeft();
+			}
+			else
+			{
 				if (yourGuess == goalNum)
 				{
 					winProcess();
-					break;
-				}
-				else if (yourGuess > goalNum)
-				{
-					printLine(lowerMessage);
-					System.out.println(goalNum + ", " + numGuessesThis + ", " + numGuessesAll);
-					guessesLeft(numGuessesThis);
-				}
-				else if (yourGuess < goalNum)
-				{
-					printLine(higherMessage);
-					System.out.println(goalNum + ", " + numGuessesThis + ", " + numGuessesAll);
-					guessesLeft(numGuessesThis);
-				}
-				else
-				{
-					printLine("Not a guess");
 				}
 			}
+			return yourGuess != goalNum;
+		}
+		return false;
+	}
+
+	/**
+	 * @return: returns true if guesses are less than max guesses, false if not.
+	 */
+	private boolean totalTest() 
+	{
+		if (numGuessesThis < MAX_GUESSES)
+		{
+			return true;
 		}
 		else 
 		{
 			printLine(loseMessage);
+			return false;
 		}
 	}
 
-	private void guessesLeft(int numGuesses) 
+	/**
+	 * Prints line telling how many guesses are left.
+	 */
+	private void guessesLeft() 
 	{	
-		printLine("You have " + (MAX_GUESSES - numGuesses) + " guesses left.");
-	}
-	
-	//private boolean firstAskTest()
-	//{
-		
-	//}
+		if (numGuessesThis < MAX_GUESSES)
+		{
+			printLine("You have " + (MAX_GUESSES - numGuessesThis) + " guesses left.");
+		}
+		else
+		{
 
+		}
+	}
+
+	/**
+	 * Prints how many guesses it took to win and changes best score.
+	 */
 	private void winProcess() 
 	{
 		System.out.println("You guessed it in " + numGuessesThis + " guess(es)");
-		if (numGuessesThis <= bestScore)
+		if (bestScore == 0)
 		{
-			numGuessesThis = bestScore;	
+			bestScore = numGuessesThis;
+		}
+		else if (numGuessesThis <= bestScore)
+		{
+			bestScore = numGuessesThis;	
 		}
 	}
 
+	/**
+	 * @return: returns random number that acts as goal number for guesser.
+	 */
 	private int makeGoalNum() 
 	{
 		return rGen.nextInt(100) + 1;
 	}
 	
+	/**
+	 * @return: returns yes answer if user wants to play again an increments game counter.
+	 */
 	public boolean playAgain()
 	{
-		String answer = readLine("Play again?").toLowerCase();
-		if (!answer.equals("y"))
-		{
-			gameResults();
-			return false;
+		playAgain = "";
+		if (numGames == 0)
+		{	
+			playAgain = "y";
+			if (playAgain.equals("y"))
+				{
+					numGames++;
+				}
+		return playAgain.equals("y");
 		}
-		return answer.equals("y");
+		else
+		{
+			playAgain = readLine("Want to play again? Y or N: ").toLowerCase().trim();
+
+			if (playAgain.equals("y"))
+			{
+				numGames++;
+				numGuessesThis = 0;
+			}
+		return playAgain.equals("y");
+		}
 	}
 	
 	//make a readLine method
 	public String readLine(String prompt)
 	{
 		System.out.print(prompt);
-		return in.nextLine();
+		return in.next();
 	}
 	
+	//printline method.
 	public void printLine(String text)
 	{
 		System.out.println(text);
@@ -173,6 +228,9 @@ public class GuessingGame
 		return in.nextInt();
 	}
 	
+	/**
+	 * @return: returns guess from user and increments counters.
+	 */
 	public int Guess()
 	{
 		numGuessesThis++;
@@ -180,6 +238,9 @@ public class GuessingGame
 		return readInt("Your guess? ");
 	}
 	
+	/**
+	 * @param args: main run method.
+	 */
 	public static void main(String[] args)
 	{
 		GuessingGame program = new GuessingGame();
